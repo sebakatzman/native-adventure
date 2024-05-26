7bd441aa1ea46c294b848ee3143cb726cc686e84
Sebastian <k.sebastianariel@gmail.com>
2024-05-26 17:32:50 -0300

    front apuntando a back de prod.

--- front/next.config.js
+++ front/next.config.js
@@ -4,9 +4,10 @@ const nextConfig = {
       domains: ['localhost', '127.0.0.1', 'panelnativeadventure.pythonanywhere.com', '825620b6-6de9-4993-8356-dbde62e1afe5.booqable.shop'],
     },    
     env: {
-      base_url: 'http://127.0.0.1:8000/api/'
+      //base_url: 'http://127.0.0.1:8000/api/'
       //base_url: 'https://panelnativeadventure.pythonanywhere.com/api/' 
-      //base_url: 'https://native-adventure.com/api/'
+      base_url: 'https://native-adventure.com/api/',
+      //base_url: 'https://testing-native-adventure.com/api/'
     },
 }
 module.exports = nextConfig;
