41f2d6f4215f4dc652105626ca651541edfa5a35
Sebastian <k.sebastianariel@gmail.com>
2024-07-13 01:18:33 -0300

    fix next.config

--- front/next.config.js
+++ front/next.config.js
@@ -4,10 +4,7 @@ const nextConfig = {
       domains: ['localhost', '127.0.0.1', 'testing.native-adventure.com', 'panelnativeadventure.pythonanywhere.com', '825620b6-6de9-4993-8356-dbde62e1afe5.booqable.shop'],
     },    
     env: {
-      base_url: 'http://127.0.0.1:8000/api/'
-      //base_url: 'https://panelnativeadventure.pythonanywhere.com/api/' 
-      //base_url: 'https://native-adventure.com/api/',
-      //base_url: 'https://testing-native-adventure.com/api/'
+      base_url: 'https://testing-native-adventure.com/api/'
     },
 }
 module.exports = nextConfig;
